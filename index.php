91881f341bbd738c9e086dc718b9f53ddb5905d3
plotiwitolp <plotiwitolp@gmail.com>
2022-09-11 19:26:46 +0400

    update

--- index.php
+++ index.php
@@ -11,7 +11,47 @@
 </head>
 
 <body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href="/">
+                    <img src="https://e7.pngegg.com/pngimages/197/457/png-clipart-round-multicolored-logo-vanamo-logo-icons-logos-emojis-tech-companies.png" alt="logo">
+                </a>
+            </div>
+            <ul id="menu">
+                <li><a href="#">test 1</a></li>
+                <li><a href="#">test 2</a></li>
+                <li><a href="#">test 3</a></li>
+                <li><a href="#">test 4</a></li>
+                <li><a href="#">test 5</a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="content">
+                <h1>First title</h1>
+                <p class="go">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat laborum rerum minus atque voluptas sunt velit! Totam nobis commodi cum voluptatibus necessitatibus esse, repellendus, laboriosam eum dignissimos tempora nihil eos!</p>
 
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga
+                <div class="gos"> laudantium quibusdam accusantium quasi voluptatum perspiciatis pariatur ut hic</div> consequuntur repellat quos possimus, recusandae nobis distinctio asperiores in voluptatibus! Iure, eaque!</p>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat laborum rerum minus atque voluptas sunt velit! Totam nobis commodi cum voluptatibus necessitatibus esse, repellendus, laboriosam eum dignissimos tempora nihil eos!</p>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga laudantium quibusdam accusantium quasi voluptatum perspiciatis pariatur ut hic consequuntur repellat quos possimus, recusandae nobis distinctio asperiores in voluptatibus! Iure, eaque!</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam porro quos deleniti rerum hic neque ullam ipsa, veritatis accusantium minus iusto? Vel laudantium modi quaerat, vero earum ab amet aperiam.</p>
+                <img src="https://wallup.net/wp-content/uploads/2019/09/27326-shelby-cobra-vehicles-cars-auto-muscle-race-racing-silver-chrome-wheels-hot-rod.jpg" alt="">
+                <img src="https://avatars.mds.yandex.net/i?id=e3ed0e37f75e719b6ebf4c4448d66027-5256069-images-thumbs&n=13&exp=1" alt="">
+                <img src="https://image.winudf.com/v2/image/Y29tLkxpdmVXYWxscGFwZXJzVUEuYXBwMDM4OF9zY3JlZW5zaG90c180XzIyYzEzNTdj/screen-4.jpg?fakeurl=1&type=.jpg" alt="">
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <?= date('Y'); ?> &copy; all right reserved, John Carpenter
+        </div>
+    </footer>
     <script src="./assets/js/jquery-3.6.0.min.js"></script>
     <script src="./assets/js/script.js"></script>
 </body>
